41018d98dded052890c031cd771c5d78a38c2dec
flofgi <83550218+flofgi@users.noreply.github.com>
2025-06-11 22:55:51 +0200

    Add files via upload

---
+++ ex1serv.sh
@@ -0,0 +1,3 @@
+date | nc -l -s localhost -p 12345
+$0
+
---
+++ ex2serv.sh
@@ -0,0 +1,20 @@
+rm ./fifo
+mkfifo ./fifo
+
+function interpret (){
+  date
+  while read line;
+  do
+    echo "Debug, received == $line ==" >&2
+    if [ $line == "exit" ]; then
+       echo "Bye bye"
+       exit
+    fi
+    $line >&1
+  done 
+}
+
+echo "start."
+nc -l -s localhost -p 12345 < ./fifo | interpret > ./fifo
+echo "End."
+$0
