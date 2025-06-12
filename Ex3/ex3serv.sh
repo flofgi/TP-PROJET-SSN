a7d136aa900dfbf23eaec5e3b5fcb8d44df2cb52
flofgi <83550218+flofgi@users.noreply.github.com>
2025-06-12 14:29:18 +0200

    Réarrangement dans l'ordre

---
+++ Ex3/ex3serv.sh
@@ -0,0 +1,28 @@
+rm ./fifo
+mkfifo ./fifo
+
+function interpret (){
+  date
+  echo "entrer le mot de passe"
+  while read line;
+  do
+  mdp=$(cat config )
+  if [ $line == $mdp ]; then
+  echo "Vous etes connecté"
+  while read line;
+  do
+     echo "Debug, received == $line ==" >&2
+     if [ $line == "exit" ]; then
+        echo "Bye bye"
+        exit
+     fi
+     $line >&1
+   done
+  fi 
+  done
+}
+
+echo "start."
+nc -l -s localhost -p 12345 < ./fifo | interpret > ./fifo
+echo "End."
+$0
